8b24fd5677af07bfc910cc0a716e240f39ffc62c
KSVarun <ksvarun001@gmail.com>
2019-12-24 15:12:10 +0530

    header processing

--- src/main/java/com/esri/account/controller/AccountController.java
+++ src/main/java/com/esri/account/controller/AccountController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class AccountController {
@@ -29,8 +33,13 @@ public class AccountController {
 
 
     @GetMapping("/")
-    public ResponseEntity<?> findAllGame(@PageableDefault Pageable pageable, HttpServletRequest request){
+    public ResponseEntity<?> findAllGame(@PageableDefault Pageable pageable,
+                                      //   @RequestHeader Map<String, String> header,
+                                         HttpServletRequest request){
         String key=request.getHeader("X-API-Key");
+//        List<String> keys = new ArrayList<>();
+//        header.forEach((k,v)->{keys.add(k);keys.add(v);});
+//        System.out.println("value------>"+keys.stream().collect(Collectors.toList()));
         if (key==null){
             throw new ApiKeyException("Missing request header X-API-Key");
         }
@@ -48,7 +57,7 @@ public class AccountController {
         return new ResponseEntity<String>("BAD_REQUEST",HttpStatus.BAD_REQUEST);
     }
 
-    @PostMapping("/newAccountData")
+    @PostMapping("/")
     public ResponseEntity<?> addNewGame(@Valid @RequestBody Account account, BindingResult result){
         ResponseEntity<?> errorMap = validationErrorService.ValidationService(result);
         if(errorMap!=null) return errorMap;
